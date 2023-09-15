024327d1b2404fce5868f2aafe2785cb7567bdfa
gf8023wmm01 <592422790@qq.com>
2023-09-15 09:01:44 +0800

    添加eslint插件

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import type { indexType } from './config/config'
-import sum from '@/config/fun'
+import type { indexType } from "./config/config";
+import sum from "@/config/fun";
 const sayHello = (index: indexType): number => {
-    console.log('hello')
-    return sum(index)
-}
+  console.log("hello");
+  return sum(index);
+};
 
-export default sayHello
+export default sayHello;
